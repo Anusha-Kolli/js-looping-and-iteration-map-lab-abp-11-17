1e5de86bd39fdd0e902581596dd0a2bf9c54db1c
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-01-12 02:29:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ function lowerCaseDrivers (drivers) {
 }
 
 function nameToAttributes(drivers){
-  
+  return drivers.map(function (drivers){
+    const driverNameFirst = driverName.split(' ')[0];
+    const driverNamelast = driverName.split(' ')[1];
+  })
 }

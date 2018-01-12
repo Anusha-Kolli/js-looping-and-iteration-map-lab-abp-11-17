e110b6d9f93f762e57684ac19565043a020d3c5b
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-01-12 02:11:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function lowerCaseDrivers (drivers, callback) {
   const newArr = [];
 
   for (const element of drivers) {
-    newArr.push(callback(element));
+    toLowerCase(newArr.push(callback(element)));
   }
 
   return newArr;

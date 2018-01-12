38a1e56bfc3d85a4ac4780fdb9fc381f211762eb
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-01-12 02:14:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 // Code your solution in this file.
 function lowerCaseDrivers (drivers, callback) {
 
-  for (const element of drivers) {
-    newArr.push(callback(element));
-  }
 
-  return newArr;
 }
